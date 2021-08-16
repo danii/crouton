7fa41d040f662d17b459644f232f9fea5039f73b
ibx34 <Guamjust@gmail.com>
2021-08-16 00:10:25 -0400

    feat(clap): add clap to running crouton

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,44 @@
-// use crouton::command; // Unused...
+use clap::{App, SubCommand};
+use crouton::command;
 use crouton::crouton::Crouton;
-use shadow_rs::shadow;
+use shadow_rs::shadow; // Add `Arg` back when I need it...
 
 shadow!(build);
 
 fn main() {
-    clearscreen::clear().unwrap();
-    let mut application = Crouton::new("{dir} {status} {time}");
-    application.start();
+    let app = App::new(build::PROJECT_NAME)
+        .about("A simple, small shell made in Rust.")
+        .version(build::PKG_VERSION)
+        .subcommand(SubCommand::with_name("test").about("Tests the new command system..."));
 
-    // let cmd = command::Command::new("owo",|a| match a {
-    //     Some(args) => {
-    //         for a in args {
-    //             println!("Arg: {}", a);
-    //         }
-    //     }
-    //     None => {}
-    // });
-    // println!(
-    //     "Name: {}\n{:?}",
-    //     cmd.name,
-    //     (cmd.func)(Some(vec![
-    //         String::from("owo"),
-    //         String::from("ewe"),
-    //         String::from("uwu"),
-    //         String::from("pwp")
-    //     ]))
-    // );
+    let matches = app.get_matches();
+
+    match matches.subcommand() {
+        ("test", _) => {
+            #[allow(clippy::single_match)]
+            let cmd = command::Command::new("owo", |a| match a {
+                Some(args) => {
+                    for a in args {
+                        println!("Arg: {}", a);
+                    }
+                }
+                None => {}
+            });
+            println!(
+                "Name: {}\n{:?}",
+                cmd.name,
+                (cmd.func)(Some(vec![
+                    String::from("owo"),
+                    String::from("ewe"),
+                    String::from("uwu"),
+                    String::from("pwp")
+                ]))
+            );
+        }
+        _ => {
+            clearscreen::clear().unwrap();
+            let mut application = Crouton::new("{dir} {status} {time}");
+            application.start();
+        }
+    }
 }
