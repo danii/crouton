bb53e8876f177f8e6c4671f4fd7513e574ed0a95
ibx34 <Guamjust@gmail.com>
2021-08-16 14:41:27 -0400

    update(main): start to add build_info command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::App;
+use clap::{App, SubCommand};
 use crouton::crouton::Crouton;
 use shadow_rs::shadow; // Add `Arg` back when I need it...
 
@@ -7,14 +7,24 @@ shadow!(build);
 fn main() {
     let app = App::new(build::PROJECT_NAME)
         .about("A simple, small shell made in Rust.")
-        .version(build::PKG_VERSION);
+        .version(build::PKG_VERSION)
+        .subcommand(SubCommand::with_name("build_info").about("Runs and shows build info."));
 
     let matches = app.get_matches();
 
-    matches.subcommand();
-    {
-        clearscreen::clear().unwrap();
-        let mut application = Crouton::new("{dir} {status} {time}");
-        application.start();
+    match matches.subcommand() {
+        ("build_info", _) => {
+            clearscreen::clear().unwrap();
+            println!("Build info\n\n");
+
+            let mut application = Crouton::new("{dir} {status} {time}");
+            application.start();
+        }
+
+        _ => {
+            clearscreen::clear().unwrap();
+            let mut application = Crouton::new("{dir} {status} {time}");
+            application.start();
+        }
     }
 }
